c592603c0c487d2281c95f62c0a04945bdee82bb
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-06-22 09:32:28 -0500

    Added a navbar with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import UserResultsContainer from "./components/UserResultsContainer.js";
 import Search from "./components/Search/Search.js";
+import Header from "./components/Header/Header.js";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Search />
       <UserResultsContainer />
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <h1>User Directory</h1>
+    </nav>
+  );
+};
+
+export default Header;
