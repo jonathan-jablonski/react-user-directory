aec82487b0bfd1489ab3069a9362b30e7413f7a9
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-06-21 19:32:11 -0500

    Search bar appears on the page!

---
+++ src/components/Search/Search.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+function Search(props) {
+  return (
+    <div className="text-center">
+      <form className="col d-flex" onSubmit={props.handleFormSubmit}>
+        <input
+          className="form-control"
+          value={props.value}
+          name="search"
+          onChange={props.handleInputChange}
+          type="search"
+          placeholder="Search by first name"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Search;
